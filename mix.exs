621dd973aa68d9e39a7162cead2738ae726cbd34
pragdave <dave@pragprog.com>
2014-07-26 11:03:20 -0500

    Forgot to bump the version number

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Earmark.Mixfile do
   def project do
     [
       app:         :earmark,
-      version:     "0.1.6",
+      version:     "0.1.7",
       elixir:      "~> 0.14.3",
       escript:     escript_config,
       deps:        deps,
