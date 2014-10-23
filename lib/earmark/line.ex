c0e0dcdf97b8406932875651e1db11717a8e258d
Stuart Coyle <stuart.coyle@gmail.com>
2014-10-24 07:51:59 +1100

    Allow code fence blocks to be indented.

--- lib/earmark/line.ex
+++ lib/earmark/line.ex
@@ -114,7 +114,7 @@ defmodule Earmark.Line do
         [ _, spaces, code ] = match
         %Indent{level: div(String.length(spaces), 4), content: code }
 
-      match = Regex.run(~r/^(```|~~~)\s*(\S*)/, line) ->
+      match = Regex.run(~r/^\s*(```|~~~)\s*(\S*)/, line) ->
         [ _, fence, language ] = match
         %Fence{delimiter: fence, language: language}
 
