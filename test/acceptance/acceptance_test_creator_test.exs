9f21bcb2398b3ef417d33e96b07f2c3db90d5346
RobertDober <robert.dober@gmail.com>
2015-10-31 14:37:24 +0100

    acceptance_test creation refactored, all acceptance tests finished

--- test/acceptance/acceptance_test_creator_test.exs
+++ test/acceptance/acceptance_test_creator_test.exs
@@ -1,21 +1,21 @@
 defmodule AcceptanceTestCreator do
   use ExUnit.Case
 
+  alias Support.AcceptanceTest
+
   {:ok, test_case_data } =
     Path.join([__DIR__,"../assets/tests.json"])
     |> File.stream!( [], :line )
     |> Enum.reject( &(String.match?(&1, ~r{^\s*#})) )
     |> Enum.join( "\n" )
-    |> Poison.Parser.parse( keys: :atoms ) # We are operating on a json file that is part of the application and contains
-    # only four different keys, so keys: :atoms is safe. Nevertheless one might think about decoding the json into a nice
-    # struct...
+    |> Poison.decode( as: [AcceptanceTest] )
 
                                            
-  for %{section: section, example: example, markdown: markdown, html: html} <- test_case_data do
-   @tag :"example_#{example}"
-    test "Acceptance Tests -- Section #{section} (#{example})" do
-      result = Earmark.to_html unquote(markdown), %Earmark.Options{smartypants: false}
-      assert result == unquote(html)
+  for acceptance_test <- test_case_data do
+   @tag :"example_#{acceptance_test.example}"
+    test "Acceptance Tests -- Section #{acceptance_test.section} (#{acceptance_test.example})" do
+      result = Earmark.to_html unquote(acceptance_test.markdown), %Earmark.Options{smartypants: false}
+      assert result == unquote(acceptance_test.html)
     end
   end
 
