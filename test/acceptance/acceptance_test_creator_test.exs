ab0e91e4dfa7921494b6986ea21eba8078df96a6
RobertDober <robert.dober@gmail.com>
2015-12-20 12:56:02 +0100

    got rid of stderr in acceptance tests

--- test/acceptance/acceptance_test_creator_test.exs
+++ test/acceptance/acceptance_test_creator_test.exs
@@ -4,18 +4,22 @@ defmodule AcceptanceTestCreatorTest do
   alias Support.AcceptanceTestStruct
 
   {:ok, test_case_data } =
-    Path.join([__DIR__,"../assets/tests.json"])
-    |> File.stream!( [], :line )
-    |> Enum.reject( &(String.match?(&1, ~r{^\s*#})) )
-    |> Enum.join( "\n" )
-    |> Poison.decode( as: [AcceptanceTestStruct] )
+  Path.join([__DIR__,"../assets/tests.json"])
+  |> File.stream!( [], :line )
+  |> Enum.reject( &(String.match?(&1, ~r{^\s*#})) )
+  |> Enum.join( "\n" )
+  |> Poison.decode( as: [AcceptanceTestStruct] )
 
-                                           
+
   for acceptance_test <- test_case_data do
     @tag :acceptance 
     @tag :"example_#{acceptance_test.example}"
     test "Acceptance Tests -- Section #{acceptance_test.section} (#{acceptance_test.example})\n---\n#{acceptance_test.markdown}\n---\n" do
-      result = Earmark.to_html unquote(acceptance_test.markdown), %Earmark.Options{smartypants: false}
+
+      result = IgnoreStderr.in_fun fn ->
+        Earmark.to_html unquote(acceptance_test.markdown), %Earmark.Options{smartypants: false}
+      end
+
       assert result == unquote(acceptance_test.html)
     end
   end
