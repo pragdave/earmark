c42bf0c739f0ed30679ca9298991b2c4fe43dc95
RobertDober <robert.dober@gmail.com>
2015-12-21 09:07:40 +0100

    trying to fix travis

--- test/acceptance/acceptance_test_creator_test.exs
+++ test/acceptance/acceptance_test_creator_test.exs
@@ -2,7 +2,6 @@ defmodule AcceptanceTestCreatorTest do
   use ExUnit.Case
 
   alias Support.AcceptanceTestStruct
-  import IgnoreOutput
 
   {:ok, test_case_data } =
   Path.join([__DIR__,"../assets/tests.json"])
@@ -17,8 +16,8 @@ defmodule AcceptanceTestCreatorTest do
     @tag :"example_#{acceptance_test.example}"
     test "Acceptance Tests -- Section #{acceptance_test.section} (#{acceptance_test.example})\n---\n#{acceptance_test.markdown}\n---\n" do
 
-      result = ignore_stderr do 
-        Earmark.to_html unquote(acceptance_test.markdown), %Earmark.Options{smartypants: false}
+      result = IgnoreOutput.in_fun :standard_error, fn ->
+        Earmark.to_html( unquote(acceptance_test.markdown), %Earmark.Options{smartypants: false} )
       end
 
       assert result == unquote(acceptance_test.html)
