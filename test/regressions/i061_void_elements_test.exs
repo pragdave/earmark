28fcb53882aca480a85194d5c79960560cc49345
RobertDober <robert.dober@gmail.com>
2016-03-28 21:02:38 +0200

    specing 61

--- test/regressions/i061_void_elements_test.exs
+++ test/regressions/i061_void_elements_test.exs
@@ -2,10 +2,10 @@ defmodule Regressions.I061VoidElementsTest do
   use ExUnit.Case
   import ExUnit.CaptureIO
 
-  @i61_void_elements ~s{<img src="whatever.png">}
+  @i61_void_elements ~s{<img src="whatever.png"></img>\n**strong**}
   test "Issue: https://github.com/pragdave/earmark/issues/61" do
     result = Earmark.to_html @i61_void_elements
-    assert result == ~s[<img src="whatever.png">\n]
+    assert result == ~s[<img src=\"whatever.png\">\n<p><strong>strong</strong></p>\n]
   end
 
   test "Issue: https://github.com/pragdave/earmark/issues/61 no message" do
