2c0c22e3493a1abedf23a7e9a32cf6cd6d689eca
RobertDober <robert.dober@gmail.com>
2016-06-25 12:47:12 +0200

    Options{line} behavior speced

--- test/parser/error_test.exs
+++ test/parser/error_test.exs
@@ -10,10 +10,22 @@ defmodule Parser.ErrorTest do
     end) == "<no file>:3: warning: Unexpected line =\n"
   end
 
+  test "Unexpected line, with lnb" do 
+    assert capture_io( :stderr, fn->
+      Earmark.to_html "A\nB\n=", %Options{line: 42}
+    end) == "<no file>:44: warning: Unexpected line =\n"
+  end
+
   test "Closing Backtick" do 
     assert capture_io( :stderr, fn->
       Earmark.to_html "A\n`B\n"
     end) == "<no file>:2: warning: Closing unclosed backquotes ` at end of input\n"
+  end
+
+  test "Closing Backtick, with lnb" do 
+    assert capture_io( :stderr, fn->
+      Earmark.to_html "A\n`B\n", %Options{line: 42}
+    end) == "<no file>:43: warning: Closing unclosed backquotes ` at end of input\n"
   end
 
   test "Closing Backtick chained" do
@@ -22,10 +34,22 @@ defmodule Parser.ErrorTest do
     end) == "<no file>:3: warning: Closing unclosed backquotes ``` at end of input\n"
   end
 
+  test "Closing Backtick chained, with lnb" do
+    assert capture_io( :stderr, fn ->
+      Earmark.to_html "one\n`\n`` ` ```", %Options{line: 42}
+    end) == "<no file>:44: warning: Closing unclosed backquotes ``` at end of input\n"
+  end
+
   test "Closing Backtick in list" do
     assert capture_io( :stderr, fn ->
       Earmark.to_html "* one\n* two\n* `three\nfour", %Options{file: "list.md"}
     end) == "list.md:3: warning: Closing unclosed backquotes ` at end of input\n"
+  end
+
+  test "Closing Backtick in list, with lnb" do
+    assert capture_io( :stderr, fn ->
+      Earmark.to_html "* one\n* two\n* `three\nfour", %Options{file: "list.md", line: 24}
+    end) == "list.md:26: warning: Closing unclosed backquotes ` at end of input\n"
   end
 
   test "Closing Backtick before list" do
@@ -34,16 +58,34 @@ defmodule Parser.ErrorTest do
     end) == "<no file>:2: warning: Closing unclosed backquotes `` at end of input\n"
   end
 
+  test "Closing Backtick before list, with lnb" do
+    assert capture_io( :stderr, fn ->
+      Earmark.to_html "one`\n* two ` ``", %Options{line: 42}
+    end) == "<no file>:43: warning: Closing unclosed backquotes `` at end of input\n"
+  end
+
   test "Failed to find closing tag" do 
     assert capture_io( :stderr, fn ->
       Earmark.to_html "one\ntwo\n<three>\nfour", %Options{file: "input_file.md"}
     end) == "input_file.md:3: warning: Failed to find closing <three>\n"
   end
 
+  test "Failed to find closing tag, with lnb" do 
+    assert capture_io( :stderr, fn ->
+      Earmark.to_html "one\ntwo\n<three>\nfour", %Options{file: "input_file.md", line: 23}
+    end) == "input_file.md:25: warning: Failed to find closing <three>\n"
+  end
+
   test "Opening Backtick inside list" do
     assert capture_io( :stderr, fn ->
       Earmark.to_html "* `"
     end) == "<no file>:1: warning: Closing unclosed backquotes ` at end of input\n"
+  end
+
+  test "Opening Backtick inside list, with lnb" do
+    assert capture_io( :stderr, fn ->
+      Earmark.to_html "* `", %Options{line: 42}
+    end) == "<no file>:42: warning: Closing unclosed backquotes ` at end of input\n"
   end
 
   test "Closing Backtick after list" do
--- test/regressions/i066_error_context_test.exs
+++ test/regressions/i066_error_context_test.exs
@@ -19,4 +19,10 @@ defmodule Regressions.I066ErrorContextTest do
       Earmark.to_html ~s(* And\n* `Hello\n* World)
     end) == "<no file>:2: warning: Closing unclosed backquotes ` at end of input\n"
   end
+
+  test "Issue https://github.com/pragdave/earmark/issues/66 3" do
+    assert capture_io( :stderr, fn->
+      Earmark.to_html ~s(* And\n* `Hello\n* World), %Earmark.Options{line: 42}
+    end) == "<no file>:43: warning: Closing unclosed backquotes ` at end of input\n"
+  end
 end
