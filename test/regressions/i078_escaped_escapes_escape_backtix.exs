4389c7228f331f67ef0f850e867418aa261f4ddb
RobertDober <robert.dober@gmail.com>
2016-06-30 21:06:14 +0200

    issue 78, tests added

---
+++ test/regressions/i078_escaped_escapes_escape_backtix.exs
@@ -0,0 +1,35 @@
+defmodule Regressions.I078EscapedEscapesEscapeBacktix do
+  use ExUnit.Case
+  import ExUnit.CaptureIO
+
+  defp open_file(filename) do
+    case File.open(filename, [:utf8]) do
+      {:ok, device} -> device 
+      {:error, reason} -> IO.puts(:stderr, "Unable to open file: #{filename}, #{reason}")
+    end
+  end
+
+  defp html_from_file(filename) do 
+    IO.stream( open_file(filename), :line)
+    |> Enum.to_list()
+    |> Earmark.to_html()
+  end
+
+  test "Issue https://github.com/pragdave/earmark/issues/77 broken markdown" do 
+
+    assert capture_io( :stderr, fn->
+      # Broken code in line 24
+      html_from_file("test/fixtures/i077_broken.md")
+    end) == "<no file>:42: warning: Closing unclosed backquotes ` at end of input\n"
+    # Yes this is correct unless we forbid multiline inline code blocks the error
+    # cannot be detected earlier
+  end
+
+
+  test "Issue https://github.com/pragdave/earmark/issues/77 fixed markdown" do 
+    assert capture_io( :stderr, fn->
+      # Fixed code in line 24
+      html_from_file("test/fixtures/i077_fixed.md")
+    end) == ""
+  end
+end
