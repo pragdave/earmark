f9b2f935c5e859a5182e0239e8ab37c2b3278feb
RobertDober <robert.dober@gmail.com>
2016-07-04 20:40:00 +0200

    1.3 test name issue

--- test/regressions/i066_error_context_test.exs
+++ test/regressions/i066_error_context_test.exs
@@ -20,7 +20,7 @@ defmodule Regressions.I066ErrorContextTest do
     end) == "<no file>:2: warning: Closing unclosed backquotes ` at end of input\n"
   end
 
-  test "Issue https://github.com/pragdave/earmark/issues/66 3" do
+  test "Issue https://github.com/pragdave/earmark/issues/66 4" do
     assert capture_io( :stderr, fn->
       Earmark.to_html ~s(* And\n* `Hello\n* World), %Earmark.Options{line: 42}
     end) == "<no file>:43: warning: Closing unclosed backquotes ` at end of input\n"
