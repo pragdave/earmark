07362c1269ee69a9eb28a25edde338f9222c5358
RobertDober <robert.dober@gmail.com>
2016-09-27 19:28:04 +0200

    made tests Elixir 1.2 compatible

--- test/earmark_helpers_tests/link_parser_test.exs
+++ test/earmark_helpers_tests/link_parser_test.exs
@@ -3,110 +3,105 @@ defmodule EarmarkHelpersTests.LinkParserTest do
 
   alias Earmark.Helpers.LinkParser
 
-  describe "text part" do
-    test "empty" do 
+  # describe "text part" do
+    test "text part: empty" do 
       assert {~s<[]()>, "", "", nil} == LinkParser.parse_link("[]()")
     end
-    test "incorrect" do 
+    test "text part: incorrect" do 
       assert nil == LinkParser.parse_link("([]")
       assert nil == LinkParser.parse_link("([]()")
     end
-    test "simple text" do 
+    test "text part: simple text" do 
       assert {~s<[hello]()>, ~s<hello>, ~s<>, nil} == LinkParser.parse_link("[hello]()")
     end
-    test "text with escapes" do 
+    test "text part: text with escapes" do 
       assert {~s<[hello[]()>, ~s<hello[>, ~s<>, nil} == LinkParser.parse_link("[hello\\[]()")
     end
-    test "text with many parts" do 
+    test "text part: text with many parts" do 
       assert {~s<[hello( world])]()>, ~s<hello( world])>, ~s<>, nil} == LinkParser.parse_link("[hello( world\\])]()")
     end
-    test "simple imbrication" do 
+    test "text part: simple imbrication" do 
       assert {~s<[[hello]]()>, ~s<[hello]>, ~s<>, nil} == LinkParser.parse_link("[[hello]]()")
     end
-    test "complex imbrication" do 
+    test "text part: complex imbrication" do 
       assert {~s<[pre[iniside]suff]()>, ~s<pre[iniside]suff>, ~s<>, nil} == LinkParser.parse_link("[pre[iniside]suff]()")
     end
-    test "deep imbrication" do 
+    test "text part: deep imbrication" do 
       assert {~s<[pre[[in]]side])]()>, ~s<pre[[in]]side])>, ~s<>, nil} == LinkParser.parse_link("[pre[[in\\]]side])]()")
     end
-    test "missing closing brackets" do 
+    test "text part: missing closing brackets" do 
       assert nil ==  LinkParser.parse_link("[pre[[in\\]side])]")
     end
-  end
+  # end
 
-  describe "url part" do
-    test "incorrect" do 
+  # describe "url part" do
+    test "url part: incorrect" do 
       assert nil == LinkParser.parse_link("[](")
       assert nil == LinkParser.parse_link("[text](url")
     end
-    test "simple url" do 
+    test "url part: simple url" do 
       assert {~s<[text](url)>, ~s<text>, ~s<url>, nil} == LinkParser.parse_link("[text](url)")
     end
-    test "url with escapes" do 
+    test "url part: url with escapes" do 
       assert {~s<[text](url))>, ~s<text>, ~s<url)>, nil} == LinkParser.parse_link("[text](url\\))")
     end
-    test "double )) at end" do 
+    test "url part: double )) at end" do 
       assert {~s<[text](url)>, ~s<text>, ~s<url>, nil} == LinkParser.parse_link("[text](url))")
     end
-    test "url with many parts" do 
+    test "url part: url with many parts" do 
       assert {~s<[text](pre[()>, ~s<text>, ~s<pre[(>, nil} == LinkParser.parse_link("[text](pre[\\()")
     end
-    test "simple imbrication" do 
+    test "url part: simple imbrication" do 
       assert {~s<[text]((url))>, ~s<text>, ~s<(url)>, nil} == LinkParser.parse_link("[text]((url))")
     end
-    test "complex imbrication" do 
+    test "url part: complex imbrication" do 
       assert {~s<[text](pre](in fix)suff)>, ~s<text>, ~s<pre](in fix)suff>, nil} == LinkParser.parse_link("[text](pre](in fix)suff)")
     end
-    test "deep imbrication" do 
+    test "url part: deep imbrication" do 
       assert {~s<[text](a(1)[((2) \\one)z)>, ~s<text>, ~s<a(1)[((2) \\one)z>, nil} == LinkParser.parse_link("[text](a(1)[((2) \\\\one)z)")
     end
-    test "missing closing parens" do 
+    test "url part: missing closing parens" do 
       assert nil ==  LinkParser.parse_link("[text](")
     end
-  end
+  # end
 
-  describe "url part with title" do
-    test "simple url" do 
+  # describe "url part with title" do
+    test "url part with title: simple url" do 
       assert {~s<[text](url 'title')>, ~s<text>, ~s<url>, ~s<title>} == LinkParser.parse_link("[text](url 'title')")
       assert {~s<[text](url  "title")>, ~s<text>, ~s<url>, ~s<title>} == LinkParser.parse_link(~s<[text](url  "title")>)
     end
 
-    test "title escapes parens" do 
+    test "url part with title: title escapes parens" do 
       assert {~s<[text](url "(title")>, ~s<text>, ~s<url>, ~s<(title>} == LinkParser.parse_link(~s<[text](url "(title")>)
       assert {~s<[text](url "tit)le")>, ~s<text>, ~s<url>, ~s<tit)le>} == LinkParser.parse_link(~s<[text](url "tit)le")>)
     end
+  # end
 
-  end
-
-  describe "deprecate in v1.1, remove in v1.2" do
-    test "remove in v1.2" do 
+  # describe "deprecate in v1.1, remove in v1.2" do
+    test "deprecated:  remove in v1.2" do 
       assert {~s<[text](url "title')>, ~s<text>, ~s<url>, ~s<title>} == LinkParser.parse_link(~s<[text](url "title')>)
       assert {~s<[text](url 'title")>, ~s<text>, ~s<url>, ~s<title>} == LinkParser.parse_link(~s<[text](url 'title")>)
       src = ~s<[text](url 'title')title")title")>
       assert {src, ~s<text>, ~s<url>, ~s<title')title")title>} == LinkParser.parse_link(src)
     end
-    test "title quotes cannot be escaped" do 
+    test "deprecated:  title quotes cannot be escaped" do 
       assert {~s<[text](url "title\\')>, ~s<text>, ~s<url>, ~s<title\\>} == LinkParser.parse_link(~s<[text](url "title\\')>)
       assert {~s<[text](url 'title\\")>, ~s<text>, ~s<url>, ~s<title\\>} == LinkParser.parse_link(~s<[text](url 'title\\")>)
     end
-  end
+  # end
 
-  describe "url no title" do
-    test "missing space" do 
+  # describe "url no title" do
+    test "url no title: missing space" do 
       assert {~s<[text](url'title')>, ~s<text>, ~s<url'title'>, nil} == LinkParser.parse_link("[text](url'title')")
       assert {~s<[text](url"title")>, ~s<text>, ~s<url"title">, nil} == LinkParser.parse_link(~s<[text](url"title")>)
     end
-    test "no title even before v1.2" do 
+    test "url no title: no title even before v1.2" do 
       assert {~s<[text](url"title')>, ~s<text>, ~s<url"title'>, nil} == LinkParser.parse_link(~s<[text](url"title')>)
       assert {~s<[text](url'title")>, ~s<text>, ~s<url'title"> , nil} == LinkParser.parse_link(~s<[text](url'title")>)
     end
-    test "missing second quote" do 
+    test "url no title: missing second quote" do 
       assert {~s<[text](url "title)>, ~s<text>, ~s<url "title>, nil} == LinkParser.parse_link(~s<[text](url "title)>)
     end
-  end
+  # end
 
-  describe "complex example" do
-
-  end
-  
 end
--- test/regressions/i088_anchor_links_inside_parens_test.exs
+++ test/regressions/i088_anchor_links_inside_parens_test.exs
@@ -1,32 +1,32 @@
 defmodule Regressions.I88AnchorLinksInsideParensTest do
   use ExUnit.Case
 
-  describe "parens" do
-    test "minimal case" do 
+  # describe "parens" do
+    test "parens: minimal case" do 
       result = Earmark.to_html("([]())")
       assert "<p>(<a href=\"\"></a>)</p>\n" == result
     end
-    test "normal case" do 
+    test "parens: normal case" do 
       result = Earmark.to_html( "([text](link))" )
       assert "<p>(<a href=\"link\">text</a>)</p>\n" == result
     end
-    test "non regression" do 
+    test "parens: non regression" do 
       result = Earmark.to_html( "[text](link)" )
       assert "<p><a href=\"link\">text</a></p>\n" == result
     end
-    test "non regression on titles" do 
+    test "parens: non regression on titles" do 
       result = Earmark.to_html( "[text](link 'title')still title'))" )
       assert ~s{<p><a href="link" title="title&#39;)still title">text</a>)</p>\n} == result
     end
 
-    test "images" do 
+    test "parens: images" do 
       result = Earmark.to_html "(![text](src))"
       assert ~s{<p>(<img src="src" alt="text"/>)</p>\n} == result
     end
     
-    test "images with titles" do 
+    test "parens: images with titles" do 
       result = Earmark.to_html( "![text](src 'title')still title'))" )
       assert ~s{<p><img src="src" alt="text" title="title&#39;)still title"/>)</p>\n} == result
     end
-  end
+  # end
 end
