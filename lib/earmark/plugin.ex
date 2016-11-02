404087dfd0dae312205e57c8e92f3089580761a6
RobertDober <robert.dober@gmail.com>
2016-11-02 07:56:32 +0100

    working on doc

--- lib/earmark/plugin.ex
+++ lib/earmark/plugin.ex
@@ -7,13 +7,60 @@ defmodule Earmark.Plugin do
     def exception(msg), do: %__MODULE__{message: msg}
   end
 
+  @moduledoc """
+  Plugins are modules that implement a render function. Right now that is `as_html`.
+
+  #### API
+
+  ##### Plugin Registration
+
+  When invoking `Earmark.as_html(some_md, options)` we can register plugins inside the `plugins` map, where
+  each plugin is a value pointed to by the prefix triggering it.
+
+  Prefixes are appended to `"$$"` and lines starting by that string will be rendered by the registered plugin.
+
+  `%Earmark.Options{plugins: %{"" => CommentPlugin}}` would trigger the `CommentPlugin` for each block of
+  lines prefixed by `$$`, while `%Earmark.Options{plugins: %{"cp" => CommentPlugin}}` would do the same for
+  blocks of lines prefixed by `$$cp`.
+
+  Please see the documentation of `Plugin.define` for a convenience function that helps creating the necessary
+  `Earmark.Options` structs for the usage of plugins.
+
+  ##### Plugin Invocation
+
+  `as_html` (or other render functions in the future) is invoked with a list of pairs containing the text
+  and line number of the lines in the block. As an example, if our plugin was registered with the default prefix
+  of `""` and the markdown to be converted was:
+
+        # Plugin output ahead
+        $$ line one
+        $$
+        $$ line two
+
+  `as_html` would be invoked as follows:
+
+        as_html([{"line one", 2}, {"", 3}, {"line two", 4})
+
+  ##### Plugin Output
+
+  Earmark's render function will invoke the plugin's render function as explained above. It can then integrate the
+  return value of the function into the generated rendering output if it complies to the following criteria.
+
+  1. It returns a string
+  1. It returns a list of strings
+  1. It returns a pair of lists containing a list of strings and a list of error/warning tuples.
+  Where the tuples are of the form `{:error | :warning, line_number, descriptive_text}`
+
+
+  """
+
   @doc """
   adds the definition of one or more plugins to `Earmark.Options`.
 
   If the plugin is defined with the default prefix and no other options are needed
   one can use the one parameter form:
 
-      iex> Earmark.Plugin.define(Earmark) # not a legal plugin ofg course
+      iex> Earmark.Plugin.define(Earmark) # not a legal plugin of course
       %Earmark.Options{plugins: %{"" => Earmark}}
 
   More then one plugin can be defined, as long as all prefixes differ
@@ -22,7 +69,6 @@ defmodule Earmark.Plugin do
       ...> defmodule P2, do: nil
       ...> Earmark.Plugin.define([ Earmark, {P1, "p1"}, {P2, "p2"} ])
       %Earmark.Options{plugins: %{"" => Earmark, "p1" => Unit.PluginTest.P1, "p2" => Unit.PluginTest.P2}}
-
 
   """
   def define(plugin_defs)
