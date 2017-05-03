509d8f6c2672d313e33d0f2311b264eb33b3474b
pragdave <dave@pragdave.me>
2017-05-03 12:25:18 -0500

    opinionate mix.exs :)

--- mix.exs
+++ mix.exs
@@ -3,41 +3,45 @@ Code.eval_file "tasks/readme.exs"
 defmodule Earmark.Mixfile do
   use Mix.Project
 
+  @version   "1.2.1"
+  
+  @deps  [
+    { :credo,    "~> 0.6", only: [ :dev, :test ] },
+    { :dialyxir, "~> 0.4", only: [ :dev, :test ] },
+  ]
+
+  @description """
+    Earmark is a pure-Elixir Markdown converter.
+
+    It is intended to be used as a library (just call Earmark.as_html),
+    but can also be used as a command-line tool (run mix escript.build
+    first).
+
+    Output generation is pluggable.
+    """
+
+  ############################################################
+  
   def project do
     [
-      app:          :earmark,
-      version:      "1.2.1",
-      elixir:       "~> 1.4",
+      app:           :earmark,
+      version:       @version,
+      elixir:        "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       escript:       escript_config(),
-      deps:          deps(),
-      description:   description(),
+      deps:          @deps,
+      description:   @description,
       package:       package(),
     ]
   end
 
   def application do
-    [applications: []]
-  end
-
-  defp deps do
     [
-      { :credo,    "~> 0.6", only: [ :dev, :test ] },
-      { :dialyxir, "~> 0.4", only: [ :dev, :test ] },
-   ]
+      applications: []
+    ]
   end
 
-  defp description do
-    """
-    Earmark is a pure-Elixir Markdown converter.
 
-    It is intended to be used as a library (just call Earmark.as_html),
-    but can also be used as a command-line tool (just run mix escript.build
-    first).
-
-    Output generation is pluggable.
-    """
-  end
 
   defp package do
     [
