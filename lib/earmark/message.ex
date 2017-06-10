13cc21764c95af30dfe222bf2bb7b910e7ce64bb
RobertDober <robert.dober@gmail.com>
2017-06-10 09:37:08 +0200

    wip

--- lib/earmark/message.ex
+++ lib/earmark/message.ex
@@ -11,10 +11,10 @@ defmodule Earmark.Message do
   @spec add_message(container_type, ts) :: container_type
   def add_message(container, message)
   def add_message(options = %Options{}, message) do 
-    %{options | messages: [message | options.message]}
+    %{options | messages: [message | options.messages]}
   end
   def add_message(context = %Context{}, message) do 
-    %{context | options: %{context.options | messages: [message | context.options.message]}}
+    %{context | options: %{context.options | messages: [message | context.options.messages]}}
   end
 
   def emit_messages(filename, messages, device \\ :stderr), do:
