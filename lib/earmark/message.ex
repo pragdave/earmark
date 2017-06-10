788868149f29d832867c94388d8cbc7704950e32
RobertDober <robert.dober@gmail.com>
2017-06-10 09:04:35 +0200

    wip

--- lib/earmark/message.ex
+++ lib/earmark/message.ex
@@ -1,9 +1,21 @@
 defmodule Earmark.Message do
 
+  alias Earmark.Context
+  alias Earmark.Options
+
   @type message_type :: :error | :warning
   @type t :: {message_type, number, binary}
   @type ts:: list(t)
+  @type container_type :: %Options{} | %Context{}
 
+  @spec add_message(container_type, ts) :: container_type
+  def add_message(container, message)
+  def add_message(options = %Options{}, message) do 
+    %{options | messages: [message | options.message]}
+  end
+  def add_message(context = %Context{}, message) do 
+    %{context | options: %{context.options | messages: [message | context.options.message]}}
+  end
 
   def emit_messages(filename, messages, device \\ :stderr), do:
     Enum.each(messages, &(emit_message(filename, &1, device)))
---
+++ test/support/parser.ex
@@ -0,0 +1,5 @@
+defmodule Support.Parser do
+  def parse_lines lines do
+    Earmark.Parser.parse(%Earmark.Options{}, lines, false)
+  end
+end
