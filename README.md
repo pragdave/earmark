f923a454b5b5428a3f7e52cfd4a0b44f4d369a63
RobertDober <robert.dober@gmail.com>
2018-03-08 21:00:09 +0100

    fixed typos from issue #161, c.f. corresponding branch pls

--- README.md
+++ README.md
@@ -63,20 +63,6 @@ Standard [Gruber markdown][gruber].
 
 ## Extensions
 
-### Github Flavored Markdown
-
-
-As GFM is a moving target, Earmark does not support all of GFM, here is a list of what is supported:
-
-* Syntax Highlightening
-
-Yes the generated code blocks have a corresponding `class` attribute:
-
-  iex(1)> Earmark.as_html "```elixir
-   [] |> Enum.into(%{})"
-  {:ok, "<pre><code class="elixir">   [] |&gt; Enum.into(%{})</code></pre>
-",
-
 
 ### Tables
 
--- lib/earmark.ex
+++ lib/earmark.ex
@@ -58,18 +58,6 @@ defmodule Earmark do
 
   ## Extensions
 
-  ### Github Flavored Markdown
-  
-
-  As GFM is a moving target, Earmark does not support all of GFM, here is a list of what is supported:
-
-  * Syntax Highlightening
-
-  Yes the generated code blocks have a corresponding `class` attribute:
-
-    iex(1)> Earmark.as_html "```elixir\n   [] |> Enum.into(%{})"
-    {:ok, "<pre><code class=\"elixir\">   [] |&gt; Enum.into(%{})</code></pre>\n",
-
 
   ### Tables
 
@@ -102,7 +90,7 @@ defmodule Earmark do
 
   HTML attributes can be added to any block-level element. We use
   the Kramdown syntax: add the line `{:` _attrs_ `}` following the block.
-
+ 
   _attrs_ can be one or more of:
 
   * `.className`
