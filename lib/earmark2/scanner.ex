989c727da5d92214b00ba9698f0fc8a2a718203e
RobertDober <robert.dober@gmail.com>
2018-08-15 21:43:42 +0200

    Earmark v2, scanning automatized with a macro

--- lib/earmark2/scanner.ex
+++ lib/earmark2/scanner.ex
@@ -1,6 +1,7 @@
 defmodule Earmark2.Scanner do
 
-  import Earmark.Helpers.LeexHelpers, only: [tokenize: 2]
+  use Earmark2.Scanner.Macros
+  # import Earmark2.Scanner.Macros
 
   @moduledoc """
   An interface to the leex lexer `src/token_lexer.xrl`
@@ -23,112 +24,36 @@ defmodule Earmark2.Scanner do
 
   """
   def scan(line) do
-    tokenize(line, with: :token_lexer) 
+    tokenize(line, []) 
   end
 
 
-  defp compile_token({desc, symbol}) do
-    {Regex.compile("\A#{desc}(.*)\z"), symbol}
+  defp tokenize(line, tokens)
+  defp tokenize("", tokens), do: tokens |> Enum.reverse
+  defp tokenize(line, tokens) do
+    with {token, rest} <- get_token(line), do: tokenize(rest, [token|tokens])
   end
 
-  defp match_token({regx, symbol}, line) do
-    cond  Regex.run(regx}
-  end
-  defp next_token(line) do
-    tokens
-    |> Enum.find_value(&match_token(&1, line))
-  end
 
-  defp tokens do
-    [
-      {"\\\\", :backslash},
-      {"\\^", :caret},
-    ] |> Enum.map(&compile_token/1)
-  end
+  @backslash ~r<\A\\\z>
+  @escaped ~r<\A\\(.)(.*)>
+  deftoken :dquote,"\"" 
+  deftoken :hashes, "#+"
+  deftoken :name, "\\p{L}[\\p{L}\\p{N}]*"
+  deftoken :number, "[-+]?\\p{N}+(?:\\.\\p{N}*)?"
+  deftoken :stars, "\\*+"
+  deftoken :ws,    "\\s+"
 
-# BACKTICKS   = `+
-# CARET       = \^
-# COLON       = :
-# DASHES      = -+
-# DQUOTE      = "
-# EQUALS      = =+
-# GT          = >+
-# HASHES      = #+
-# LACCOLADE   = \{
-# LBRACKET    = \[
-# LPAREN      = \(
-# LT          = <+
-# RACCOLADE   = \}
-# RBRACKET    = \]
-# RPAREN      = \)
-# SLASHES     = /+
-# SQUOTE      = '
-# SYMBOLS     = [&|@$?;,%!§]+
-# STARS       = \*+
-# TILDES      = ~+
-# UNDERSCORES = _+
-# WHITESPACE  = \s+
-
-# REST        = [[^\][\(\)-><*_+\s\t"'\\\{\}&|@$?;,%!§]
-# TRAILING    = [^\][\(\)><*+\s\t"'\\\{\}&|@^$?;,%!§]
-# ALPHANUM    = {rest}{trailing}*
-
-# Rules.
-
-# {BACKSLASH} : {token, {backslash, tokenline, tokenchars}}.
-# {BACKSLASH}. : {token, {verbatim, tokenline, dismiss_backslash(tokenchars)}}.
-# {BACKTICKS} : {token, {backticks, tokenline, tokenchars}}.
-# {CARET} : {token, {caret, tokenline, tokenchars}}.
-# {COLON} : {token, {colon, tokenline, tokenchars}}.
-# {DASHES} : {token, {dashes, tokenline, tokenchars}}.
-# {DQUOTE} : {token, {dquote, tokenline, tokenchars}}.
-# {EQUALS} : {token, {equals, tokenline, tokenchars}}.
-# {GT} : {token, {gt, tokenline, tokenchars}}.
-# {HASHES} : {token, {hashes, tokenline, tokenchars}}.
-# {LACCOLADE} : {token, {laccolade, tokenline, tokenchars}}.
-# {LBRACKET} : {token, {lbracket, tokenline, tokenchars}}.
-# {LPAREN} : {token, {lparen, tokenline, tokenchars}}.
-# {LT} : {token, {lt, tokenline, tokenchars}}.
-# {RACCOLADE} : {token, {raccolade, tokenline, tokenchars}}.
-# {RBRACKET} : {token, {rbracket, tokenline, tokenchars}}.
-# {RPAREN} : {token, {rparen, tokenline, tokenchars}}.
-# {SLASHES} : {token, {slashes, tokenline, tokenchars}}.
-# {SQUOTE} : {token, {squote, tokenline, tokenchars}}.
-# {STARS} : {token, {stars, tokenline, tokenchars}}.
-# {SYMBOLS} : {token, {symbols, tokenline, tokenchars}}.
-# {TILDES} : {token, {tildes, tokenline, tokenchars}}.
-# {UNDERSCORES} : {token, {underscores, tokenline, tokenchars}}.
-# {WHITESPACE} : {token, {whitespace, tokenline, tokenchars}}.
-
-# {ALPHANUM} : {token, {verbatim, tokenline, tokenchars}}.
-
-  def tokenize(line, table, tokens)
-  def tokenize("", _, tokens), do: [{:eol, ""}|tokens] |> Enum.reverse
-  def tokenize(line, table, tokens) do
-    with {token, rest} <- get_token(line, "", 0), do: tokenize(rest, table, [token|tokens])
-  end
-
-  @digit ~r{\d}
-  def get_token(codepoints, acc, state)
-  def get_token([cp|tail], [], :init) do
+  defp get_token(line) do
+    match(line) ||
     cond do
-      cp == "+" -> {{:plus, "+"}, tail}
-      cp == "#" -> {{:hash, "#"}, tail}
-      Regex.match?(@digit)
+      Regex.run(@backslash, line)   -> {{:backslash, "\\"}, ""}
+      m = Regex.run(@escaped, line) -> 
+        with [_, escaped, rest] <- m, do: {{:verbatim, escaped}, rest}
+      true                          -> {{:error, line}, ""}
     end
   end
-  def get_token(["+"|tail], _acc, :init), do: {{:plus, "+"}, tail}
-  def get_token(["#"|tail], _acc, :init), do: {{:hash, "#"}, tail}
-  def get_token([" "|tail], _acc, :init), do: get_token(tail, [" "], :ws) 
-  def get_token([cp|tail], _acc, :init) do
-    cond do
-      Set.member?(Set.new(["0","1","2","3","4","5","6","7","8","9"]), cp ) -> get_token(tail, [cp], :num)
-      _                                                                    -> get_token(tail, [cp], :alnum) 
 
-    end
-  end
-  def get_token([" "|tail], acc, :ws), do: get_token(tail, [" "|acc], :ws)
-  def get_token(codepoints, acc, :ws), do: {{:ws, acc}, codepoints}
 
 end
 
