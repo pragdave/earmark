57882a879ff3fac1e387eeaef1e8af814c8e13c5
RobertDober <robert.dober@gmail.com>
2018-11-13 08:09:33 +0100

    Inline code Commonmark conforming acceptance tests

--- test/acceptance/inline_code_test.exs
+++ test/acceptance/inline_code_test.exs
@@ -4,10 +4,9 @@ defmodule Acceptance.InlineCodeTest do
   import Support.Helpers, only: [as_html: 1]
 
   describe "Inline Code" do
-
     test "plain simple" do
       markdown = "`foo`\n"
-      html     = "<p><code class=\"inline\">foo</code></p>\n"
+      html = "<p><code class=\"inline\">foo</code></p>\n"
       messages = []
 
       assert as_html(markdown) == {:ok, html, messages}
@@ -15,7 +14,7 @@ defmodule Acceptance.InlineCodeTest do
 
     test "plain simple, right?" do
       markdown = "`hi`lo`\n"
-      html     = "<p><code class=\"inline\">hi</code>lo`</p>\n"
+      html = "<p><code class=\"inline\">hi</code>lo`</p>\n"
       messages = [{:warning, 1, "Closing unclosed backquotes ` at end of input"}]
 
       assert as_html(markdown) == {:error, html, messages}
@@ -23,7 +22,7 @@ defmodule Acceptance.InlineCodeTest do
 
     test "this time you got it right" do
       markdown = "`a\nb`c\n"
-      html     = "<p><code class=\"inline\">a\nb</code>c</p>\n"
+      html = "<p><code class=\"inline\">a\nb</code>c</p>\n"
       messages = []
 
       assert as_html(markdown) == {:ok, html, messages}
@@ -36,13 +35,12 @@ defmodule Acceptance.InlineCodeTest do
 
       assert as_html(markdown) == {:ok, html, messages}
     end
-
   end
 
   describe "Inline Code with escapes" do
     test "a lone escaped backslash" do
       markdown = "`\\\\`"
-      html ="<p><code class=\"inline\">\\\\</code></p>\n"
+      html = "<p><code class=\"inline\">\\\\</code></p>\n"
       messages = []
 
       assert as_html(markdown) == {:ok, html, messages}
@@ -50,7 +48,7 @@ defmodule Acceptance.InlineCodeTest do
 
     test "with company" do
       markdown = "`hello \\\\ world`"
-      html ="<p><code class=\"inline\">hello \\\\ world</code></p>\n"
+      html = "<p><code class=\"inline\">hello \\\\ world</code></p>\n"
       messages = []
 
       assert as_html(markdown) == {:ok, html, messages}
@@ -59,10 +57,45 @@ defmodule Acceptance.InlineCodeTest do
     test "unescaped escape" do
       markdown = "`\\`"
       html = "<p><code class=\"inline\">\\</code></p>\n"
-      messages =  [{:warning, 1, "Closing unclosed backquotes ` at end of input"}]
+      messages = []
 
       assert as_html(markdown) == {:error, html, messages}
     end
+  
+    test "backtix cannot be escaped" do 
+      markdown = "`` \\` ``"
+      html = "<p><code class=\"inline\">\\</code></p>\n"
+      messages = []
+
+      assert as_html(markdown) == {:error, html, messages}
+    end
+  end
+
+  describe "whitespace treatment" do
+    test "squashing" do
+      markdown = "`alpha   beta`"
+      html = "<p><code class=\"inline\">alpha beta</code></p>\n"
+      messages = []
+
+      assert as_html(markdown) == {:error, html, messages}
+    end
+
+    test "remove at start" do
+      markdown = "`  alpha beta`"
+      html = "<p><code class=\"inline\">alpha beta</code></p>\n"
+      messages = []
+
+      assert as_html(markdown) == {:error, html, messages}
+    end
+
+    test "remove and squash" do
+      markdown = "`  alpha   beta `"
+      html = "<p><code class=\"inline\">alpha beta</code></p>\n"
+      messages = []
+
+      assert as_html(markdown) == {:error, html, messages}
+    end
+
   end
 end
 
