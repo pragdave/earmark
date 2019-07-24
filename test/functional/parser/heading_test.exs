cf3da957a697e623d92c62f67e94ad8d2a329dbc
Mohamed Boudra <boudra.moha@gmail.com>
2019-07-24 20:54:58 +0200

    add test for :start_heading_level

--- test/functional/parser/heading_test.exs
+++ test/functional/parser/heading_test.exs
@@ -14,6 +14,17 @@ defmodule Parser.HeadingTest do
     assert result == [%Block.Heading{content: "Heading", level: 1, lnb: 2}]
   end
 
+  test "Start heading option is respected" do
+    result = Earmark.as_html!("# test", %Earmark.Options{start_heading_level: 2})
+    expected = "<h2>test</h2>\n"
+    assert result == expected
+  end
+
+  test "Default start heading option is 1" do
+    result = Earmark.as_html!("# test", %Earmark.Options{})
+    expected = "<h1>test</h1>\n"
+    assert result == expected
+  end
 end
 
 # SPDX-License-Identifier: Apache-2.0
