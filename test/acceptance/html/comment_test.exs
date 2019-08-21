593669d362e8baa50b9306875e7801161a852c6f
RobertDober <robert.dober@gmail.com>
2019-08-21 14:59:50 +0200

    Added missing acceptance tests for HTML Comments

---
+++ test/acceptance/html/comment_test.exs
@@ -0,0 +1,31 @@
+defmodule Acceptance.Html.CommentTest do
+  use ExUnit.Case
+
+  import Support.Helpers, only: [as_html: 1]
+
+  describe "HTML Comments" do
+    test "one line" do
+      markdown = "<!-- Hello -->"
+      html     = "<!-- Hello -->"
+      messages = []
+
+      assert as_html(markdown) == {:ok, html, messages}
+    end
+
+    test "more lines" do
+      markdown = "<!-- Hello\n World -->"
+      html     = "<!-- Hello\n World -->"
+      messages = []
+
+      assert as_html(markdown) == {:ok, html, messages}
+    end
+
+    test "what about the closing" do
+      markdown = "<!-- Hello\n World -->garbish"
+      html     = "<!-- Hello\n World -->garbish"
+      messages = []
+
+      assert as_html(markdown) == {:ok, html, messages}
+    end
+  end
+end
