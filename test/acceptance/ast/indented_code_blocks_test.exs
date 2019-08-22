b42098987b4e1d4ad890e0d5fe89761dc78f2245
RobertDober <robert.dober@gmail.com>
2019-08-22 17:30:26 +0200

    Working on as_ast :: html renderer...
    Renderer:
    Para, Heading, Ial, List, Code, Link, Image, Ruler, Html, HtmlOneline, HtmlComment, Footnotes, IndentedCode
    Inline:
    text, em, strong, escape, code, link, no_link, autolink, footnote, img, pure_link, hard_line_breaks

--- test/acceptance/ast/indented_code_blocks_test.exs
+++ test/acceptance/ast/indented_code_blocks_test.exs
@@ -1,48 +1,54 @@
 defmodule Acceptance.Ast.IndentedCodeBlocksTest do
   use ExUnit.Case
-  
-  import Support.Helpers, only: [as_html: 1]
+  import Support.Helpers, only: [as_ast: 1, as_html: 1]
 
-  # describe "Indented code blocks" do
+  @moduletag :ast
+
+  describe "Indented code blocks" do
     test "simple (but easy?)" do
       markdown = "    a simple\n      indented code block\n"
       html     = "<pre><code>a simple\n  indented code block</code></pre>\n"
+      ast      = Floki.parse(html) |> IO.inspect
       messages = []
 
-      assert as_html(markdown) == {:ok, html, messages}
+      assert as_ast(markdown) == {:ok, [ast], messages}
     end
 
     test "code is soo verbatim" do
       markdown = "    <a/>\n    *hi*\n\n    - one\n"
       html     = "<pre><code>&lt;a/&gt;\n*hi*\n\n- one</code></pre>\n"
+      ast      = Floki.parse(html) |> IO.inspect
       messages = []
 
-      assert as_html(markdown) == {:ok, html, messages}
+      assert as_ast(markdown) == {:ok, [ast], messages}
     end
 
     test "chunky bacon (RIP: Why)" do
       markdown = "    chunk1\n\n    chunk2\n  \n \n \n    chunk3\n"
       html     = "<pre><code>chunk1\n\nchunk2\n\n\n\nchunk3</code></pre>\n"
+      ast      = Floki.parse(html) |> IO.inspect
       messages = []
 
-      assert as_html(markdown) == {:ok, html, messages}
+      assert as_ast(markdown) == {:ok, [ast], messages}
     end
 
     test "foo and bar (now you are surprised!)" do
       markdown = "    foo\nbar\n"
       html     = "<pre><code>foo</code></pre>\n<p>bar</p>\n"
+      ast      = Floki.parse(html) |> IO.inspect
       messages = []
 
-      assert as_html(markdown) == {:ok, html, messages}
+      assert as_ast(markdown) == {:ok, ast, messages}
     end
 
     test "not the alpha, not the omega (gamma maybe?)" do
       markdown = "\n    \n    foo\n    \n\n"
       html = "<pre><code>foo</code></pre>\n"
+      ast      = Floki.parse(html) |> IO.inspect
       messages = []
-      assert as_html(markdown) == {:ok, html, messages}
+      assert as_ast(markdown) == {:ok, [ast], messages}
     end
-  # end
+  end
 end
 
 # SPDX-License-Identifier: Apache-2.0
