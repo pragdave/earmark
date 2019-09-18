167ea507cc5763cb4e7f1cc25d59caed638d49db
RobertDober <robert.dober@gmail.com>
2019-09-18 20:27:49 +0200

    Implementing Transform.transform...
    First basic acceptance tests work
    
       - html1/atx_headers_test ✓
       - html1/links_images/img_test ✓
       - html1/links_images/link_test ✓
       - html1/links_images/simple_pure_link_test ✓
       - html1/links_images/titles_test ✓
       - html1/block_ial_test ✓
       - html1/block_quotes_test ✓
       - html1/comment_test ✓
       - html1/diverse_test ✓
       - html1/emphasis_test ✓
       - /html1/empty_test ✓

--- test/acceptance/html1/empty_test.exs
+++ test/acceptance/html1/empty_test.exs
@@ -1,12 +1,15 @@
 defmodule Acceptance.Html1.EmptyTest do
   use ExUnit.Case, async: true
+  import Support.Html1Helpers
+  
+  @moduletag :html1
 
   test "empty" do
     markdown = ""
     html     = ""
     messages = []
 
-    assert Earmark.as_html(markdown) == {:ok, html, messages}
+    assert to_html1(markdown) == {:ok, html, messages}
   end
 
   test "almost empty" do
@@ -14,7 +17,7 @@ defmodule Acceptance.Html1.EmptyTest do
     html     = ""
     messages = []
 
-    assert Earmark.as_html(markdown) == {:ok, html, messages}
+    assert to_html1(markdown) == {:ok, html, messages}
   end
   
 end
