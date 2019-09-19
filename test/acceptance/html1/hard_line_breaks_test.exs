6d3e7c3eb6cfe8e33e133abf2ae3521f621736ad
RobertDober <robert.dober@gmail.com>
2019-09-19 18:15:32 +0200

    Implementing Transform.transform...
       - html1/atx_headers_test ✓
       - html1/links_images/img_test ✓
       - html1/links_images/link_test ✓
       - html1/links_images/simple_pure_link_test ✓
       - html1/links_images/titles_test ✓
       - html1/block_ial_test ✓
       - html1/block_quotes_test ✓
       - html1/comment_test ✓
       - html1/diverse_test ✓
       - html1/emphasis_test ✓
       - html1/empty_test ✓
       - html1/escape_test ✓
       - html1/fenced_code_blocks_test ✓
       - html1/footnotes_test ✓
       - html1/hard_line_breaks_test ✓

--- test/acceptance/html1/hard_line_breaks_test.exs
+++ test/acceptance/html1/hard_line_breaks_test.exs
@@ -1,62 +1,65 @@
 defmodule Acceptance.Html1.HardLineBreaksTest do
   use ExUnit.Case, async: true
   
-  import Support.Helpers, only: [as_html: 1, as_html: 2]
+  import Support.Html1Helpers
+  
+  @moduletag :html1
   describe "gfm" do 
     test "hard line breaks are enabled" do 
-      
       markdown = "line 1\nline 2\\\nline 3"
-      html     = "<p>line 1\nline 2<br />\nline 3</p>\n"
+      html     = para([
+        "line 1\nline 2",
+        :br,
+        "line 3" ])
       messages = []
 
-      assert as_html(markdown) == {:ok, html, messages}
+      assert to_html1(markdown) == {:ok, html, messages}
     end
 
     test "hard line breaks are enabled only inside paras" do 
-      
       markdown = "line 1\nline 2\\\n\nline 3"
-      html     = "<p>line 1\nline 2\\</p>\n<p>line 3</p>\n"
+      html     = construct([
+        {:p, nil, "line 1\nline 2\\"}, 
+        {:p, nil, "line 3"} ])
       messages = []
 
-      assert as_html(markdown) == {:ok, html, messages}
+      assert to_html1(markdown) == {:ok, html, messages}
     end
 
     test "hard line breaks are not enabled at the end" do 
-      
       markdown = "line 1\nline 2\\\n"
-      html     = "<p>line 1\nline 2\\</p>\n"
+      html     = para("line 1\nline 2\\")
       messages = []
 
-      assert as_html(markdown) == {:ok, html, messages}
+      assert to_html1(markdown) == {:ok, html, messages}
     end
   end
 
   describe "no gfm" do 
     test "hard line breaks are not enabled" do 
-      
       markdown = "line 1\nline 2\\\nline 3"
-      html     = "<p>line 1\nline 2\\\nline 3</p>\n"
+      html     = para("line 1\nline 2\\\nline 3")
       messages = []
 
-      assert as_html(markdown, gfm: false) == {:ok, html, messages}
+      assert to_html1(markdown, gfm: false) == {:ok, html, messages}
     end
 
     test "hard line breaks are enabled only inside paras" do 
-      
       markdown = "line 1\nline 2\\\n\nline 3"
-      html     = "<p>line 1\nline 2\\</p>\n<p>line 3</p>\n"
+      html     = construct([
+        {:p, nil, "line 1\nline 2\\"},
+        {:p, nil, "line 3"}])
       messages = []
 
-      assert as_html(markdown, gfm: false) == {:ok, html, messages}
+      assert to_html1(markdown, gfm: false) == {:ok, html, messages}
     end
 
     test "hard line breaks are not enabled at the end" do 
-      
       markdown = "line 1\nline 2\\\n"
-      html     = "<p>line 1\nline 2\\</p>\n"
+      html     = para("line 1\nline 2\\")
       messages = []
 
-      assert as_html(markdown, gfm: false) == {:ok, html, messages}
+      assert to_html1(markdown, gfm: false) == {:ok, html, messages}
     end
   end
 
