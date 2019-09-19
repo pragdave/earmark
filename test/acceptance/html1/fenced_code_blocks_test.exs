422157858b51784053c213e57c0bfc8bd4d78e86
RobertDober <robert.dober@gmail.com>
2019-09-19 08:06:51 +0200

    Implementing Transform.transform...
    First basic acceptance tests work
    
       - html1/atx_headers_test ✓
       - html1/links_images/img_test ✓
       - html1/links_images/link_test ✓
       - html1/links_images/simple_pure_link_test ✓
       - html1/links_images/titles_test ✓
       - html1/block_ial_test ✓
       - html1/block_quotes_test ✓
       - html1/comment_test ✓
       - html1/diverse_test ✓
       - html1/emphasis_test ✓
       - html1/empty_test ✓
       - html1/escape_test ✓
       - html1/fenced_code_blocks_test ✓

--- test/acceptance/html1/fenced_code_blocks_test.exs
+++ test/acceptance/html1/fenced_code_blocks_test.exs
@@ -1,50 +1,69 @@
 defmodule Acceptance.Html1.FencedCodeBlocksTest do
   use ExUnit.Case, async: true
 
-  import Support.Helpers, only: [as_html: 1, as_html: 2]
+  import Support.Html1Helpers
+  
+  @moduletag :html1
 
-  # describe "Fenced code blocks" do
+  describe "Fenced code blocks" do
     test "no lang" do
       markdown = "```\n<\n >\n```\n"
-      html     = "<pre><code class=\"\">&lt;\n &gt;</code></pre>\n"
+      html     = construct([
+        :pre,
+        {:code, ~s{class=""}},
+        "&lt;\n &gt;" ])
       messages = []
 
-      assert as_html(markdown) == {:ok, html, messages}
+      assert to_html1(markdown) == {:ok, html, messages}
     end
 
     test "still no lang" do
       markdown = "~~~\n<\n >\n~~~\n"
-      html     = "<pre><code class=\"\">&lt;\n &gt;</code></pre>\n"
+      html     = construct([
+        :pre,
+        {:code, ~s{class=""}},
+        "&lt;\n &gt;" ])
       messages = []
 
-      assert as_html(markdown) == {:ok, html, messages}
+      assert to_html1(markdown) == {:ok, html, messages}
     end
 
     test "elixir 's the name" do
       markdown = "```elixir\naaa\n~~~\n```\n"
-      html     = "<pre><code class=\"elixir\">aaa\n~~~</code></pre>\n"
+      html     = construct([
+        :pre,
+        {:code, ~s{class="elixir"}},
+        "aaa\n~~~" ])
       messages = []
 
-      assert as_html(markdown) == {:ok, html, messages}
+
+      assert to_html1(markdown) == {:ok, html, messages}
     end
 
     test "with a code_class_prefix" do
       markdown = "```elixir\naaa\n~~~\n```\n"
-      html     = "<pre><code class=\"elixir lang-elixir\">aaa\n~~~</code></pre>\n"
+      html     = construct([
+        :pre,
+        {:code, ~s{class="elixir lang-elixir"}},
+        "aaa\n~~~" ])
       messages = []
 
-      assert as_html(markdown, code_class_prefix: "lang-") == {:ok, html, messages}
+      assert to_html1(markdown, code_class_prefix: "lang-") == {:ok, html, messages}
     end
 
     test "look mam, more lines" do
       markdown = "   ```\naaa\nb\n  ```\n"
-      html     = "<pre><code class=\"\">aaa\nb</code></pre>\n"
+      html     = construct([
+        :pre,
+        {:code, ~s{class=""}},
+        "aaa\nb"
+      ])
       messages = []
 
-      assert as_html(markdown) == {:ok, html, messages}
+      assert to_html1(markdown) == {:ok, html, messages}
     end
 
-  # end
+  end
 end
 
 # SPDX-License-Identifier: Apache-2.0
