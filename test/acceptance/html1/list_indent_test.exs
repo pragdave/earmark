1906373f0880504bab9812d488c98cfb4704a8b5
RobertDober <robert.dober@gmail.com>
2019-09-22 09:49:54 +0200

    Implementing Transform.transform...
       - html1/atx_headers_test ✓
       - html1/links_images/img_test ✓
       - html1/links_images/link_test ✓
       - html1/links_images/simple_pure_link_test ✓
       - html1/links_images/titles_test ✓
       - html1/block_ial_test ✓
       - html1/block_quotes_test ✓
       - html1/comment_test ✓
       - html1/diverse_test ✓
       - html1/emphasis_test ✓
       - html1/empty_test ✓
       - html1/escape_test ✓
       - html1/fenced_code_blocks_test ✓
       - html1/footnotes_test ✓
       - html1/hard_line_breaks_test ✓
       - html1/horizontal_rules_test ✓
       - html1/html_test ✓
       - html1/indented_code_blocks_test ✓
       - tml1/inline_ial_test ✓
       - html1/line_breaks_test ✓
       - html1/list_indent_test.exs ✓

--- test/acceptance/html1/list_indent_test.exs
+++ test/acceptance/html1/list_indent_test.exs
@@ -1,56 +1,99 @@
 defmodule Acceptance.Html1.ListIndentTest do
   use ExUnit.Case, async: true
 
-  import Support.Helpers, only: [as_html: 1]
-
+  import Support.Html1Helpers
+  
+  @moduletag :html1
   describe "different levels of indent" do
 
     test "ordered two levels, indented by two" do
       markdown = "1. One\n  2. two"
-      html     = "<ol>\n<li><p>One</p>\n<ol start=\"2\">\n<li>two\n</li>\n</ol>\n</li>\n</ol>\n"
+      html     = construct(
+        {:ol, nil,
+          {:li, nil, [
+            {:p, nil, "One"},
+            {:ol, ~s{start="2"},
+              {:li, nil, "two"}}]}})
       messages = []
 
-      assert as_html(markdown) == {:ok, html, messages}
+      assert to_html1(markdown) == {:ok, html, messages}
     end
 
     test "mixed two levels (by 2)" do
       markdown = "1. One\n  - two\n  - three"
-      html     = "<ol>\n<li><p>One</p>\n<ul>\n<li>two\n</li>\n<li>three\n</li>\n</ul>\n</li>\n</ol>\n"
+      html     = construct(
+        {:ol, nil,
+          {:li, nil, [
+            {:p, nil, "One"},
+            {:ul, nil, [ 
+               {:li, nil, "two"},
+               {:li, nil, "three"}]}]}})
       messages = []
 
-      assert as_html(markdown) == {:ok, html, messages}
+      assert to_html1(markdown) == {:ok, html, messages}
     end
 
     test "mixed two levels (by 4)" do
       markdown = "1. One\n    - two\n    - three"
-      html     = "<ol>\n<li><p>One</p>\n<ul>\n<li>two\n</li>\n<li>three\n</li>\n</ul>\n</li>\n</ol>\n"
+      html     = construct(
+        {:ol, nil, 
+           {:li, nil, [
+             {:p, nil, "One"},
+             {:ul, nil, [
+               {:li, nil, "two"},
+               {:li, nil, "three"}]}]}})
       messages = []
 
-      assert as_html(markdown) == {:ok, html, messages}
+      assert to_html1(markdown) == {:ok, html, messages}
     end
 
     test "2 level correct pop up" do
       markdown = "- 1\n  - 1.1\n    - 1.1.1\n  - 1.2"
-      html     = "<ul>\n<li><p>1</p>\n<ul>\n<li><p>1.1</p>\n<ul>\n<li>1.1.1\n</li>\n</ul>\n</li>\n<li>1.2\n</li>\n</ul>\n</li>\n</ul>\n"
+      html     = construct(
+        {:ul, nil,
+           {:li, nil, [
+              {:p, nil, "1"},
+              {:ul, nil, [
+                {:li, nil, [
+                  {:p, nil, "1.1"},
+                  {:ul, nil, {:li, nil, "1.1.1"}}]},
+                {:li, nil, "1.2"}]}]}})
+
       messages = []
 
-      assert as_html(markdown) == {:ok, html, messages}
+      assert to_html1(markdown) == {:ok, html, messages}
     end
 
     test "mixed level correct pop up" do
       markdown = "- 1\n  - 1.1\n      - 1.1.1\n  - 1.2"
-      html     = "<ul>\n<li><p>1</p>\n<ul>\n<li><p>1.1</p>\n<ul>\n<li>1.1.1\n</li>\n</ul>\n</li>\n<li>1.2\n</li>\n</ul>\n</li>\n</ul>\n"
+      html     = construct(
+        {:ul, nil,
+           {:li, nil, [
+              {:p, nil, "1"},
+              {:ul, nil, [
+                {:li, nil, [
+                  {:p, nil, "1.1"},
+                  {:ul, nil, {:li, nil, "1.1.1"}}]},
+                {:li, nil, "1.2"}]}]}})
       messages = []
 
-      assert as_html(markdown) == {:ok, html, messages}
+      assert to_html1(markdown) == {:ok, html, messages}
     end
 
     test "4 level correct pop up" do
       markdown = "- 1\n    - 1.1\n        - 1.1.1\n    - 1.2"
-      html     = "<ul>\n<li><p>1</p>\n<ul>\n<li><p>1.1</p>\n<ul>\n<li>1.1.1\n</li>\n</ul>\n</li>\n<li>1.2\n</li>\n</ul>\n</li>\n</ul>\n"
+      html     = construct(
+        {:ul, nil,
+           {:li, nil, [
+              {:p, nil, "1"},
+              {:ul, nil, [
+                {:li, nil, [
+                  {:p, nil, "1.1"},
+                  {:ul, nil, {:li, nil, "1.1.1"}}]},
+                {:li, nil, "1.2"}]}]}})
       messages = []
 
-      assert as_html(markdown) == {:ok, html, messages}
+      assert to_html1(markdown) == {:ok, html, messages}
     end
   end
 end
