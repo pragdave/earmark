f5849b560010cb85f98542620b3b0c96eb80cac3
RobertDober <robert.dober@gmail.com>
2020-06-27 10:07:26 +0200

    i337-ast-metadata
                Specing quadruples, check DEVNOTES.md for progress
    
                DONE: hard_line_breaks, horizontal_rules
                NEXT: html/oneline html/permissive

--- DEVNOTES.md
+++ DEVNOTES.md
@@ -13,8 +13,8 @@ Specs to be rewritten:
  ✓ ├── escape_test.exs
  ✓ ├── fenced_code_blocks_test.exs
  ✓ ├── footnotes_test.exs
-   ├── hard_line_breaks_test.exs
-   ├── horizontal_rules_test.exs
+ ✓ ├── hard_line_breaks_test.exs
+ ✓ ├── horizontal_rules_test.exs
    ├── html
  ✓ │   ├── block_test.exs
    │   ├── oneline_test.exs
--- test/support/helpers.ex
+++ test/support/helpers.ex
@@ -25,9 +25,9 @@ defmodule Support.Helpers do
 
   def parse_html(html) do
     if System.get_env("DEBUG") do
-      Floki.parse(html) |> IO.inspect
+      Floki.parse(html) |> _add_4th() |> IO.inspect
     else
-      Floki.parse(html)
+      Floki.parse(html) |> _add_4th()
     end
   end
 
@@ -52,6 +52,19 @@ defmodule Support.Helpers do
   def gfm_context do
     Context.update_context(context())
   end
+
+  defp _add_4th(node)
+  defp _add_4th(nodes) when is_list(nodes) do
+    nodes
+    |> Enum.map(&_add_4th/1)
+  end
+  defp _add_4th({t, a, c}) do
+    {t, a, _add_4th(c), %{}}
+  end
+  defp _add_4th({:comment, content}) do
+    {:comment, [], content, %{comment: true}}
+  end
+  defp _add_4th(other), do: other
 
 end
 
