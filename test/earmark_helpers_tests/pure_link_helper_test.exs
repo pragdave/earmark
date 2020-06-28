4a4d24d4e3c3285b2ee5af0603c19e6f25e5362b
RobertDober <robert.dober@gmail.com>
2020-06-28 13:10:25 +0200

    i337-ast-metadata
            New AST format speced and implemented
    
            DONE: helpers tests pure_link
            NEXT: debug transformer

--- test/earmark_helpers_tests/pure_link_helper_test.exs
+++ test/earmark_helpers_tests/pure_link_helper_test.exs
@@ -2,26 +2,27 @@ defmodule Earmark.Helpers.TestPureLinkHelpers do
 
   use ExUnit.Case, async: true
   import Earmark.Helpers.PureLinkHelpers, only: [convert_pure_link: 1]
+  import EarmarkAstDsl
 
   describe "Pure Links" do
     test "nothing fancy just a plain link" do
       #                           0....+....1....+...
       result = convert_pure_link("https://a.link.com")
-      expected = {{"a", [{"href", "https://a.link.com"}], ["https://a.link.com"]}, 18}
+      expected = { tag("a", "https://a.link.com", href: "https://a.link.com"), 18}
       assert result == expected
     end
 
     test "trailing parens are not part of it" do
       #                           0....+....1....+...
       result = convert_pure_link("https://a.link.com)")
-      expected = {{"a", [{"href", "https://a.link.com"}], ["https://a.link.com"]}, 18}
+      expected = {tag("a", "https://a.link.com", href: "https://a.link.com"), 18}
       assert result == expected
     end
 
     test "trailing parens are not part of it, at least not all" do
       #                          0....+....1....+....2.
       result = convert_pure_link("(https://a.link.com))")
-      expected = {["(", {"a", [{"href", "https://a.link.com"}], ["https://a.link.com"]}, ")"], 20}
+      expected = {["(", tag("a",  "https://a.link.com", href:  "https://a.link.com"), ")"], 20}
       assert result == expected
     end
 
@@ -29,28 +30,28 @@ defmodule Earmark.Helpers.TestPureLinkHelpers do
     test "however opening parens are" do
       #                           0....+....1....+...
       result = convert_pure_link("https://a.link.com(")
-      expected = {{"a", [{"href", "https://a.link.com("}], ["https://a.link.com("]}, 19}
+      expected = {tag("a",  "https://a.link.com(", href:  "https://a.link.com("), 19}
       assert result == expected
     end
 
     test "closing parens inside are ok" do
       #                          0....+....1....+....2....+....3....+....4....+
       result = convert_pure_link("http://www.google.com/search?q=(business))+ok")
-      expected = {{"a", [{"href", "http://www.google.com/search?q=(business))+ok"}], ["http://www.google.com/search?q=(business))+ok"]}, 45}
+      expected = {tag("a",  "http://www.google.com/search?q=(business))+ok", href:  "http://www.google.com/search?q=(business))+ok"), 45}
       assert result == expected
     end
 
     test "closing parens outside are not part of it" do
       #                          0....+....1....+....2....+....3....+....4
       result = convert_pure_link("http://www.google.com/search?q=business)")
-      expected = {{"a", [{"href", "http://www.google.com/search?q=business"}], ["http://www.google.com/search?q=business"]}, 39}
+      expected = {tag("a", "http://www.google.com/search?q=business", href: "http://www.google.com/search?q=business"), 39}
       assert result == expected
     end
 
     test "closing parens can match opening parens at the end" do
       #                          0....+....1....+....2....+....3....+....4.
       result = convert_pure_link("(http://www.google.com/search?q=business)")
-      expected = {["(", {"a", [{"href", "http://www.google.com/search?q=business"}], ["http://www.google.com/search?q=business"]}, ")"], 41}
+      expected = {["(", tag("a", "http://www.google.com/search?q=business", href: "http://www.google.com/search?q=business"), ")"], 41}
       assert result == expected
     end
 
