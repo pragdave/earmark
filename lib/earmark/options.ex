1ec92f47c1d1483fca2fd5f8064a1483fc60b21e
RobertDober <robert.dober@gmail.com>
2021-09-01 17:03:48 +0200

    WIP #412 removed unused Options, unused usage of Earmark.Types and sorted options by name

--- lib/earmark/options.ex
+++ lib/earmark/options.ex
@@ -1,72 +1,50 @@
 defmodule Earmark.Options do
-  use Earmark.Types
 
-  # What we use to render
-  defstruct renderer: Earmark.HtmlRenderer,
-            # Inline style options
-            gfm: true,
-            gfm_tables: false,
+  @moduledoc """
+  This is a superset of the options that need to be passed into `EarmarkParser.as_ast/2`
+
+  The following options are proper to `Earmark` only and therefore explained in detail
+
+  - `compact_output`: boolean indicating to avoid indentation and minimize whitespace
+  - `eex`: Coming soon (EEx preprocessing)
+  - `file`: Name of file passed in from the CLI
+  - `line`: 1 but might be set to an offset for better error messages in some integration cases
+  - `ignore_strings`, `postprocessor` and `registered_processors`: processors that modify the AST returned from
+     EarmarkParser.as_ast/`2` before rendering (`post` because preprocessing is done on the markdown, e.g. `eex`)
+     Refer to the moduledoc of Earmark.`Transform` for details
+
+  All other options are passed onto EarmarkParser.as_ast/`2`
+  """
+
+  defstruct [
             breaks: false,
-            pedantic: false,
-            smartypants: true,
-            footnotes: false,
-            footnote_offset: 1,
-            wikilinks: false,
-            escape: true,
-
-            # additional prefies for class of code blocks
             code_class_prefix: nil,
-
-            # Add possibility to specify a timeout for Task.await
-            timeout: nil,
-
+            compact_output: false,
             # Internal—only override if you're brave
             do_smartypants: nil,
-
-            # Very internal—the callback used to perform
-            # parallel rendering. Set to &Enum.map/2
-            # to keep processing in process and
-            # serial
+            escape: true,
+            file: "<no file>",
+            footnote_offset: 1,
+            footnotes: false,
+            gfm: true,
+            gfm_tables: false,
+            ignore_strings: false,
+            line: 1,
             mapper: &Earmark.pmap/2,
             mapper_with_timeout: &Earmark.pmap/3,
-
-            # Filename and initial line number of the markdown block passed in
-            # for meaningfull error messages
-            file: "<no file>",
-            line: 1,
-            # [{:error|:warning, lnb, text},...]
             messages: [],
-            pure_links: true,
-            compact_output: false,
-            ignore_strings: false,
+            pedantic: false,
             postprocessor: nil,
-            registered_processors: []
+            pure_links: true,
+            registered_processors: [],
+            smartypants: true,
+            timeout: nil,
+            wikilinks: false,
+          ]
 
-
-  @type t :: %__MODULE__{
-        breaks: boolean,
-        code_class_prefix: maybe(String.t),
-        footnotes: boolean,
-        footnote_offset: number,
-        gfm: boolean,
-        pedantic: boolean,
-        pure_links: boolean,
-        smartypants: boolean,
-        wikilinks: boolean,
-        timeout: maybe(number),
-        escape: boolean
-  }
-
-  @doc false
-  # Only here we are aware of which mapper function to use!
-  def get_mapper(options) do
-    if options.timeout do
-      &options.mapper_with_timeout.(&1, &2, options.timeout)
-    else
-      options.mapper
-    end
-  end
-
+  @doc """
+  Make a legal and normalized %__MODULE__{} struct from, maps or keyword lists
+  """
   def make_options(options)
   def make_options(options) when is_list(options) do
     legal_keys =
---
+++ test/lib/options_test.exs
@@ -0,0 +1,6 @@
+defmodule Test.Lib.OptionsTest do
+  use ExUnit.Case
+
+  doctest Earmark.Options, import: true
+
+end
