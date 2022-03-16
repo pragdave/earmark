a8e7b02d78d69df7dff233b751e57184ec97ab62
RobertDober <robert.dober@gmail.com>
2022-03-16 20:47:28 +0100

    Release 1.4.22 for EarmarkParser 1.4.23

--- mix.exs
+++ mix.exs
@@ -1,13 +1,13 @@
     defmodule Earmark.Mixfile do
   use Mix.Project
 
-  @version "1.4.21"
+  @version "1.4.22"
 
   @url "https://github.com/pragdave/earmark"
 
 
   @deps [
-    {:earmark_parser, "~> 1.4.22" },
+    {:earmark_parser, "~> 1.4.23" },
     # {:earmark_parser, path: "../earmark_parser"},
     {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
     {:benchfella, "~> 0.3.0", only: [:dev]},
