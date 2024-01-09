c1d7f4ae1e45cf2ce28743cc553bc482e77774e2
Brad Hanks <hanks.brad@gmail.com>
2024-01-09 14:05:13 -0700

    added spaces after commas for readability

--- lib/earmark_parser/helpers/ast_helpers.ex
+++ lib/earmark_parser/helpers/ast_helpers.ex
@@ -1,5 +1,4 @@
 defmodule Earmark.Parser.Helpers.AstHelpers do
-
   @moduledoc false
 
   import Earmark.Parser.Ast.Emitter
@@ -10,27 +9,33 @@ defmodule Earmark.Parser.Helpers.AstHelpers do
   @doc false
   def annotate(node, from_block)
   def annotate(node, %{annotation: nil}), do: node
+
   def annotate({tag, atts, children, meta}, %{annotation: annotation}),
     do: {tag, atts, children, Map.put(meta, :annotation, annotation)}
+
   def annotate({tag, atts, children, meta}, annotation),
     do: {tag, atts, children, Map.put(meta, :annotation, annotation)}
 
   @doc false
   def attrs_to_string_keys(key_value_pair)
+
   def attrs_to_string_keys({k, vs}) when is_list(vs) do
     {to_string(k), Enum.join(vs, " ")}
   end
+
   def attrs_to_string_keys({k, vs}) do
-    {to_string(k),to_string(vs)}
+    {to_string(k), to_string(vs)}
   end
 
   @doc false
   def augment_tag_with_ial(tags, ial)
-  def augment_tag_with_ial([{t, a, c, m}|tags], atts) do
-    [{t, merge_attrs(a, atts), c, m}|tags]
+
+  def augment_tag_with_ial([{t, a, c, m} | tags], atts) do
+    [{t, merge_attrs(a, atts), c, m} | tags]
   end
+
   def augment_tag_with_ial([], _atts) do
-    [] 
+    []
   end
 
   @doc false
@@ -38,17 +43,23 @@ defmodule Earmark.Parser.Helpers.AstHelpers do
     classes =
       ["" | String.split(prefix || "")]
       |> Enum.map(fn pfx -> "#{pfx}#{language}" end)
-      {"class", classes |> Enum.join(" ")}
+
+    {"class", classes |> Enum.join(" ")}
   end
 
   @doc false
-  def codespan(text) do 
+  def codespan(text) do
     emit("code", text, class: "inline")
   end
 
   @doc false
   def render_footnote_link(ref, backref, number) do
-    emit("a", to_string(number), href: "##{ref}", id: backref, class: "footnote", title: "see footnote")
+    emit("a", to_string(number),
+      href: "##{ref}",
+      id: backref,
+      class: "footnote",
+      title: "see footnote"
+    )
   end
 
   @doc false
@@ -73,11 +84,9 @@ defmodule Earmark.Parser.Helpers.AstHelpers do
     emit("a", text, href: _encode(url))
   end
 
-
   ##############################################
   # add attributes to the outer tag in a block #
   ##############################################
-
 
   @verbatims ~r<%[\da-f]{2}>i
   defp _encode(url) do
@@ -85,7 +94,7 @@ defmodule Earmark.Parser.Helpers.AstHelpers do
     |> String.split(@verbatims, include_captures: true)
     |> Enum.chunk_every(2)
     |> Enum.map(&_encode_chunk/1)
-    |> IO.chardata_to_string
+    |> IO.chardata_to_string()
   end
 
   defp _encode_chunk([encodable, verbatim]), do: [URI.encode(encodable), verbatim]
@@ -94,6 +103,7 @@ defmodule Earmark.Parser.Helpers.AstHelpers do
   @doc false
   def merge_attrs(maybe_atts, new_atts)
   def merge_attrs(nil, new_atts), do: new_atts
+
   def merge_attrs(atts, new) when is_list(atts) do
     atts
     |> Enum.into(%{})
@@ -108,13 +118,14 @@ defmodule Earmark.Parser.Helpers.AstHelpers do
   end
 
   defp _value_merger(key, val1, val2)
+
   defp _value_merger(_, val1, val2) when is_list(val1) do
     val1 ++ [val2]
   end
+
   defp _value_merger(_, val1, val2) do
     [val1, val2]
   end
-
-
 end
+
 # SPDX-License-Identifier: Apache-2.0
