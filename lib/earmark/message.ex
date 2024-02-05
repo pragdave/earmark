a746bbb11f851ca73b9b32d67be7258e66f8678d
Brad Hanks <hanks.brad@gmail.com>
2024-02-04 22:54:59 -0700

    lib/earmark/message.ex: Earmark.Message had @spec with Earmark.Options.options() type

--- lib/earmark/message.ex
+++ lib/earmark/message.ex
@@ -1,12 +1,12 @@
 defmodule Earmark.Message do
-  @moduledoc false
-
   alias Earmark.Options
+  @moduledoc false
 
   def emit_messages(messages, %Options{file: file}) do
     messages
     |> Enum.each(&emit_message(file, &1))
   end
+
   def emit_messages(messages, proplist) when is_list(proplist) do
     messages
     |> Enum.each(&emit_message(proplist[:file], &1))
@@ -15,9 +15,11 @@ defmodule Earmark.Message do
   defp emit_message(filename, msg), do: IO.puts(:stderr, format_message(filename, msg))
 
   defp format_message(filenale, message)
+
   defp format_message(nil, {type, line, text}) do
     "<no file>:#{line}: #{type}: #{text}"
   end
+
   defp format_message(filename, {type, line, text}) do
     "#{filename}:#{line}: #{type}: #{text}"
   end
