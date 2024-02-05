286f092a743e1f671ede4d31f78311563c2bc057
Brad Hanks <hanks.brad@gmail.com>
2024-02-04 22:51:08 -0700

    lib/earmark/helpers.ex: added @spec to Earmark.Helpers.replace/4

--- lib/earmark/helpers.ex
+++ lib/earmark/helpers.ex
@@ -1,9 +1,8 @@
 defmodule Earmark.Helpers do
-
   @doc """
   `Regex.replace` with the arguments in the correct order
   """
-
+  @spec replace(String.t(), Regex.t(), String.t(), Earmark.Options.options()) :: String.t()
   def replace(text, regex, replacement, options \\ []) do
     Regex.replace(regex, text, replacement, options)
   end
