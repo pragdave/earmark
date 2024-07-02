d6dc531ffa5981ea655b6fc2b4d2ed05a612b1de
Robert Dober <robert.dober@gmail.com>
2024-07-02 09:54:21 +0200

    Fixes #495
    Removed the output of the device tuple

--- lib1/earmark/sys_interface.ex
+++ lib1/earmark/sys_interface.ex
@@ -5,14 +5,12 @@ defmodule Earmark.SysInterface do
   """
   def readlines(device_or_filename)
   def readlines(filename) when is_binary(filename) do
-    IO.inspect(filename, label: :inside)
     case File.open(filename, [:utf8]) do
       {:ok, device} -> readlines(device)
       {:error, _} = error    -> error
     end
   end
   def readlines(device) do
-    IO.inspect(device, label: :device)
     IO.stream(device, :line)
   end
 end
