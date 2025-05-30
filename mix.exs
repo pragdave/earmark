8ad072ee7c7f85214539b28199e66044a4b6f25c
Adam Millerchip <code@adamu.jp>
2025-05-30 15:56:09 +0900

    Use ex_doc directly to build docs

--- mix.exs
+++ mix.exs
@@ -10,6 +10,7 @@ defmodule Earmark.Mixfile do
     {:benchfella, "~> 0.3.0", only: [:dev]},
     {:earmark_ast_dsl, "~> 0.3.6", only: [:dev, :test]},
     {:excoveralls, "~> 0.16.0", only: [:test]},
+    {:ex_doc, "~> 0.38.2", only: [:dev]},
     # {:extractly, "~> 0.5.0", git: "https://github.com/RobertDober/extractly.git", tag: "v0.5.0-pre1", only: [:dev]},
     {:extractly, "~> 0.5.3", only: [:dev]},
     {:floki, "~> 0.21", only: [:dev, :test]},
@@ -46,7 +47,7 @@ defmodule Earmark.Mixfile do
         "coveralls.html": :test
       ],
       test_coverage: [tool: ExCoveralls],
-      aliases: [docs: &build_docs/1]
+      docs: docs()
     ]
   end
 
@@ -87,24 +88,13 @@ defmodule Earmark.Mixfile do
     ]
   end
 
-  @prerequisites """
-  run `mix escript.install hex ex_doc` and adjust `PATH` accordingly
-  """
-  defp build_docs(_) do
-    Mix.Task.run("compile")
-    ex_doc = Path.join(Mix.path_for(:escripts), "ex_doc")
-    Mix.shell().info("Using escript: #{ex_doc} to build the docs")
-
-    unless File.exists?(ex_doc) do
-      raise "cannot build docs because escript for ex_doc is not installed, make sure to \n#{@prerequisites}"
-    end
-
-    args = ["Earmark", @version, Mix.Project.compile_path()]
-    opts = ~w[--main Earmark --source-ref v#{@version} --source-url #{@url}]
-
-    Mix.shell().info("Running: #{ex_doc} #{inspect(args ++ opts)}")
-    System.cmd(ex_doc, args ++ opts)
-    Mix.shell().info("Docs built successfully")
+  defp docs() do
+    [
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @url,
+      extras: ["README.md", "RELEASE.md", "LICENSE"]
+    ]
   end
 end
 
