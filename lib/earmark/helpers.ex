b705b8461417a94fd2b1dd3701373074174af27f
Ben Murden <got.ben@gmail.com>
2025-06-21 18:48:30 +0900

    Support function returning String.t() (#510)

--- lib/earmark/helpers.ex
+++ lib/earmark/helpers.ex
@@ -2,7 +2,7 @@ defmodule Earmark.Helpers do
   @doc """
   `Regex.replace` with the arguments in the correct order
   """
-  @spec replace(String.t(), Regex.t(), String.t(), Earmark.Options.options()) :: String.t()
+  @spec replace(String.t(), Regex.t(), String.t() | (... -> String.t()), Earmark.Options.options()) :: String.t()
   def replace(text, regex, replacement, options \\ []) do
     Regex.replace(regex, text, replacement, options)
   end
